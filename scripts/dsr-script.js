97c4dcc0f48a9598c299019eda477f1521164fcc
azbadger-dev <fabricio.morenoarauz@gmail.com>
2026-05-04 20:06:34 -0500

    Document elements declaration

---
+++ scripts/dsr-script.js
@@ -0,0 +1,18 @@
+const descriptionInput = document.getElementById('description-input');
+const amountInput = document.getElementById('amount-input');
+const percentageInput = document.getElementById('percentage')
+const cashWithdrawalInput = document.getElementById('cash-withdrawal-input');
+const cashAddInput = document.getElementById('cash-add-input');
+
+const paymentMethodEntry = document.getElementById('payment-method-entry');
+const paymentMethodTable = document.getElementById('payment-method-table');
+const paymentMethodCards = document.getElementById('payment-method-cards');
+
+const btnAddSale = document.getElementById('add-sale-btn');
+const btnCashWithdrawal = document.getElementById('cash-withdrawal-btn');
+const btnCashAdd = document.getElementById('cash-add-btn');
+const btnPrevCashEdit = document.getElementById('prev-cash-edit-btn');
+const btnPrevCashSave = document.getElementById('prev-cash-save-btn');
+const btnReportOutput = document.getElementById('report-output-btn');
+
+// tarea proxima: declarar los divs pertinentes
